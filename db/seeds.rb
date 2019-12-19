53439a9c15f410be3385ac257a51d65da49bc119
Yassine Iharkachen <y.iharkachen@gmail.com>
2019-12-19 19:25:20 +0100

    Restaurant & Review models ok, validation ok, routes and views to index and show ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'Twins', category: 'Belgian',
+                  phone_number: '0222222', address: 'Uccle')
+
+Restaurant.create(name: 'Knees to Chin', category: 'Chinese',
+                  phone_number: '12345', address: 'Brussels')
+
+Restaurant.create(name: 'Villa Laurraine', category: 'French',
+                  phone_number: '0222222', address: 'Uccle')
+
+Restaurant.create(name: 'Mr Cod', category: 'Belgian',
+                  phone_number: '123456', address: 'Molenbeek')
+
+Restaurant.create(name: 'Yamato', category: 'Japanese',
+                  phone_number: '65432', address: 'Brussels')
+
+Restaurant.create(name: 'Nona', category: 'Italian',
+                  phone_number: '6543456', address: 'Brussels')
